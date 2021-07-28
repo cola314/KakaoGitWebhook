4861758ca8f78c76c3d8fe421f58a5f53dcee708
cola314 <ubj0314@naver.com>
2021-07-28 20:42:03 +0900

    https 지원 작업

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
+const https = require('https')
 const express = require('express')
 const app = express()
 const request = require('request')
-const port = 9203
+const path = require('path')
+const fs = require('fs')
+
+const PORT = 9203
+const HTTPS_PORT = 9204
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'certificate/private.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'certificate/certificate.crt')),
+  ca: fs.readFileSync('certificate/ca_bundle.crt')
+};
 
 app.use(express.json());
 
 app.post('/api/webhook/:room', (req, res) => {
-    const room = req.params.room;
+  const room = req.params.room;
 
-    if (room) {
-        var result = `${req.body.repository.name}/${req.body.ref}\n` +
-            `${req.body.commits[req.body.commits.length - 1].url}\n` +
-            `Push by ${req.body.pusher.username || req.body.pusher.name}\n\n` +
-            `${req.body.commits[req.body.commits.length - 1].message}`;
+  if (room) {
+    var result = `${req.body.repository.name}/${req.body.ref}\n` +
+      `${req.body.commits[req.body.commits.length - 1].url}\n` +
+      `Push by ${req.body.pusher.username || req.body.pusher.name}\n\n` +
+      `${req.body.commits[req.body.commits.length - 1].message}`;
 
-        console.log(result);
-        sendMessage(room, result.trim());
+    console.log(result);
+    sendMessage(room, result.trim());
 
-        return res.sendStatus(200);
-    }
+    return res.sendStatus(200);
+  }
 
-    return res.sendStatus(400);
+  return res.sendStatus(400);
 })
 
 const sendMessage = (room, message) => {
-    const options = {
-        uri: 'http://***REMOVED***:9200/send',
-        method: 'POST',
-        body: {
-            password: "4321",
-            room: room,
-            message: message
-        },
-        json: true
-    }
-    request.post(options, function (error, response, body) {
-        //post callback
-    });
+  const options = {
+    uri: 'http://***REMOVED***:9200/send',
+    method: 'POST',
+    body: {
+      password: "4321",
+      room: room,
+      message: message
+    },
+    json: true
+  }
+  request.post(options, function (error, response, body) {
+    //post callback
+  });
 }
 
-app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`)
+})
+
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+  console.log(`app listening at https://localhost:${HTTPS_PORT}`)
 })
