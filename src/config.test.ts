202dfc01b88ca038c1d43e67e517a3d233805339
cola314 <ubj0314@naver.com>
2022-06-27 22:46:34 +0900

    fix: USE_HTTPS 환경변수 "0"일때 true로 설정되는 문제 수정

--- src/config.test.ts
+++ src/config.test.ts
@@ -1,7 +1,11 @@
-import { DEV_ENV, KAKAO_BOT_API_KEY, KAKAO_BOT_SERVER_API } from './config';
+import { DEV_ENV, KAKAO_BOT_API_KEY, KAKAO_BOT_SERVER_API, USE_HTTPS } from './config';
 
 test('env loaded', () => {
   expect(DEV_ENV).toBe('1');
   expect(KAKAO_BOT_SERVER_API).not.toBeUndefined();
   expect(KAKAO_BOT_API_KEY).not.toBeUndefined();
+});
+
+test('https option set zero', () => {
+  expect(USE_HTTPS).toBe(false);
 });
--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
 // 환경변수
 export const DEV_ENV = process.env.DEV_ENV;
 
-export const USE_HTTPS = !!process.env.USE_HTTPS;
+export const USE_HTTPS = (+(process.env.USE_HTTPS) || 0) !== 0;
 export const SSL_KEY_FILE = process.env.SSL_KEY_FILE;
 export const CERT_FILE = process.env.CERT_FILE;
 export const CA_BUNDLE_FILE = process.env.CA_BUNDLE_FILE;
